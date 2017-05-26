f2584074a64dcf282027dc2fafdb001372b321bd
Norbert Nagy <nnagy@nnagys-MacBook-Pro.local>
2017-05-27 01:08:51 +0300

    Empty ios app

---
+++ HappyBike/HappyBike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappyBike
+//
+//  Created by Norbert Nagy on 27/05/2017.
+//  Copyright © 2017 Norbert Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
